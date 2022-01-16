3da02b06b0bd57a11d6efa2a04426dbd75a9ddf6
sixpl <saket@sixpl.com>
2022-01-16 23:45:32 +0530

    remove Phone input package

--- components/forms/contact-form-vertifical.js
+++ components/forms/contact-form-vertifical.js
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
+
 
 const ContactFormVertical = () => {
 
@@ -13,11 +11,6 @@ const ContactFormVertical = () => {
                     <div className="flex flex-col">
                         <input type="text" name="name" placeholder="Name" required />
                         <input type="email" name="email" placeholder="Email" required />
-                        <PhoneInput
-                            country={'us'}
-                            value={value}
-                            onChange={setValue}
-                        />
                         <input type="text" name="website" placeholder="Website" />
                         <textarea name="message" placeholder="Message" required />
                         
