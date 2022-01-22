5d57e72b8bffd937a99462395163b141662767fe
sixpl <saket@sixpl.com>
2022-01-22 06:58:49 +0530

    DM Service Start Custom Project

--- pages/services/digital-marketing/index.js
+++ pages/services/digital-marketing/index.js
@@ -22,7 +22,7 @@ const DigitalMarketingIndexPage = () => {
                                 <p className="text-2xl py-2">Avail a range of customized digital marketing services designed to win your prospects' trust</p>
                             </div>
                         </div>
-                        <div className="">
+                        <div>
                             <div className="flex flex-col lg:flex-row">
                                 <div className="lg:w-1/2 px-4 py-14">
                                     <div className="pb-4 text-center text-3xl text-slate-700">Hire #1 Digital Marketing Agency</div>
@@ -569,6 +569,76 @@ const DigitalMarketingIndexPage = () => {
                                             </div>
                                         </div>
                                         
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex flex-col lg:flex-row">
+                                <div className="lg:w-1/2 px-4 py-14">
+                                    <div className="pb-4 text-center text-3xl text-slate-700">Start Your Custom Digital Marketing Project in 4 Easy Steps</div>
+                                    <div>
+                                        <div className="flex flex-col">
+                                            <div className="flex flex-col items-center lg:flex-row justify-center">
+                                                <div className="px-4 py-4 lg:w-1/2 lg:h-52">
+                                                    <div className="flex flex-col items-center text-center p-2">
+                                                        <div className="py-2">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" className='w-20 h-20 stroke-emerald-700 stroke-1' viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M12 16v-8l-2 2" />
+                                                                <circle cx="12" cy="12" r="9" />
+                                                            </svg>
+                                                        </div>
+                                                        <div className="py-2 text-2xl text-gray-700">Define Scope of Work</div>
+                                                    </div>
+                                                </div>
+                                                <div className="px-4 py-4 lg:w-1/2 lg:h-52">
+                                                    <div className="flex flex-col items-center text-center p-2 ">
+                                                        <div className="py-2">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" className='w-20 h-20 stroke-emerald-700 stroke-1' viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M10 10a2 2 0 1 1 4 0c0 .591 -.417 1.318 -.816 1.858l-3.184 4.143l4 0" />
+                                                                <circle cx="12" cy="12" r="9" />
+                                                            </svg>
+                                                        </div>
+                                                        <div className="py-2 text-2xl text-gray-700">Mutually Decide the Payment Terms</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="flex flex-col items-center lg:flex-row justify-center">
+                                                <div className="px-4 py-4 lg:w-1/2 lg:h-52">
+                                                    <div className="flex flex-col items-center text-center p-2">
+                                                        <div className="py-2">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" className='w-20 h-20 stroke-emerald-700 stroke-1' viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M12 12a2 2 0 1 0 -2 -2" />
+                                                                <path d="M10 14a2 2 0 1 0 2 -2" />
+                                                                <circle cx="12" cy="12" r="9" />
+                                                            </svg>
+                                                        </div>
+                                                        <div className="py-2 text-2xl text-gray-700">Provide Information - Fill a Questionnaire</div>
+                                                    </div>
+                                                </div>
+                                                <div className="px-4 py-4 lg:w-1/2 lg:h-52">
+                                                    <div className="flex flex-col items-center text-center p-2">
+                                                        <div className="py-2">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" className='w-20 h-20 stroke-emerald-700 stroke-1' viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                                                <path d="M13 16v-8l-4 6h5" />
+                                                                <circle cx="12" cy="12" r="9" />
+                                                            </svg>
+                                                        </div>
+                                                        <div className="py-2 text-2xl text-gray-700">Start the Project Immediately</div>
+                                                    </div>  
+                                                </div>   
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="lg:w-1/2 px-4 py-14">
+                                    <div className="py-14">
+                                        <ContactFormVertical />
                                     </div>
                                 </div>
                             </div>
