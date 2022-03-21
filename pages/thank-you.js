fd7a235f7a57b785173a8d8b864a409feb425ce7
sixpl <saket@sixpl.com>
2022-03-21 18:26:10 +0530

    v.0.0.0.3

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,15 +1,22 @@
+import Head from 'next/head'
+
 import Layout from '../components/nav/layout'
 
 const ThankYouPage = () => {
     return (
         <Layout>
-            <div className='flex flex-col'>
-                <div className=' flex flex-col font-serif text-gray-700 items-center border-2 border-gray-200 mx-auto md:my-20 px-8 py-20'>
-                    <div className=' text-3xl py-4 text-sptheme-darkgreen'>Thank you for your interest in SixPL Services!</div>
-                    <div className=' text-2xl py-2'>Our team will get in touch with you shortly.</div>
-                    <div className=' text-xl py-2'>Office Timings: Monday-Friday (9:30am - 6:30pm IST)</div>
+            <Head>
+                <title>Thank You for Interest in SixPL Services!</title>
+            </Head>
+            <main>
+                <div className='flex flex-col'>
+                    <div className=' flex flex-col font-serif text-gray-700 items-center border-2 border-gray-200 mx-auto md:my-20 px-8 py-20'>
+                        <div className=' text-3xl py-4 text-sptheme-darkgreen'>Thank you for your interest in SixPL Services!</div>
+                        <div className=' text-2xl py-2'>Our team will get in touch with you shortly.</div>
+                        <div className=' text-xl py-2'>Office Timings: Monday-Friday (9:30am - 6:30pm IST)</div>
+                    </div>
                 </div>
-            </div>
+            </main>
         </Layout>
     )
 }
