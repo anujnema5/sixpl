fd7e731b57071e8be98f5c9ca76151a41b9e5543
sixpl <saket@sixpl.com>
2022-07-16 18:20:15 +0530

    SEO Packages in USD

--- pages/services/digital-marketing/search-engine-optimization.js
+++ pages/services/digital-marketing/search-engine-optimization.js
@@ -337,18 +337,19 @@ const SearchEngineOptimizationPage = () => {
                                                     <div>
                                                         <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6 stroke-white stroke-2' viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                            <path d="M18 5h-11h3a4 4 0 0 1 0 8h-3l6 6" />
-                                                            <line x1="7" y1="9" x2="18" y2="9" />
+                                                            <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" />
+                                                            <path d="M12 3v3m0 12v3" />
                                                         </svg>
                                                     </div>
-                                                    <div className='text-4xl font-sans'>12,500</div>
+                                                    
+                                                    <div className='text-4xl font-sans'>399</div>
                                                 </div>
                                                 <div className='pb-4 text-sm w-full bg-slate-800'>per month</div>
                                             </div>
                                             <div className='w-full'>
                                                 <div className='bg-slate-200 p-2'>
                                                     <div className='p-2 border-b-2 border-slate-300 text-xl font-semibold'>Package Summary</div>
-                                                    <div className='p-1 border-b-2 border-slate-300'>5 Keywords</div>
+                                                    <div className='p-1 border-b-2 border-slate-300'>40 Keywords</div>
                                                     <div className='p-1 border-b-2 border-slate-300'>5 Citations</div>
                                                     <div className='p-1 border-b-2 border-slate-300'>45 Backlinks</div>
                                                 </div>
@@ -389,18 +390,18 @@ const SearchEngineOptimizationPage = () => {
                                                     <div>
                                                         <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6 stroke-white stroke-2' viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                            <path d="M18 5h-11h3a4 4 0 0 1 0 8h-3l6 6" />
-                                                            <line x1="7" y1="9" x2="18" y2="9" />
+                                                            <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" />
+                                                            <path d="M12 3v3m0 12v3" />
                                                         </svg>
                                                     </div>
-                                                    <div className='text-4xl font-sans'>22,500</div>
+                                                    <div className='text-4xl font-sans'>699</div>
                                                 </div>
                                                 <div className='pb-4 text-sm w-full bg-slate-800'>per month</div>
                                             </div>
                                             <div className='w-full'>
                                                 <div className='bg-slate-200 p-2'>
                                                     <div className='p-2 border-b-2 border-slate-300 text-xl font-semibold'>Package Summary</div>
-                                                    <div className='p-1 border-b-2 border-slate-300'>10 Keywords</div>
+                                                    <div className='p-1 border-b-2 border-slate-300'>50 Keywords</div>
                                                     <div className='p-1 border-b-2 border-slate-300'>15 Citations</div>
                                                     <div className='p-1 border-b-2 border-slate-300'>100 Backlinks</div>
                                                 </div>
@@ -441,18 +442,18 @@ const SearchEngineOptimizationPage = () => {
                                                     <div>
                                                         <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6 stroke-white stroke-2' viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                            <path d="M18 5h-11h3a4 4 0 0 1 0 8h-3l6 6" />
-                                                            <line x1="7" y1="9" x2="18" y2="9" />
+                                                            <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" />
+                                                            <path d="M12 3v3m0 12v3" />
                                                         </svg>
                                                     </div>
-                                                    <div className='text-4xl font-sans'>37,500</div>
+                                                    <div className='text-4xl font-sans'>999</div>
                                                 </div>
                                                 <div className='pb-4 text-sm w-full bg-slate-800'>per month</div>
                                             </div>
                                             <div className='w-full'>
                                                 <div className='bg-slate-200 p-2'>
                                                     <div className='p-2 border-b-2 border-slate-300 text-xl font-semibold'>Package Summary</div>
-                                                    <div className='p-1 border-b-2 border-slate-300'>25 Keywords</div>
+                                                    <div className='p-1 border-b-2 border-slate-300'>60 Keywords</div>
                                                     <div className='p-1 border-b-2 border-slate-300'>25 Citations</div>
                                                     <div className='p-1 border-b-2 border-slate-300'>250 Backlinks</div>
                                                 </div>
@@ -493,19 +494,19 @@ const SearchEngineOptimizationPage = () => {
                                                     <div>
                                                         <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6 stroke-white stroke-2' viewBox="0 0 24 24" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                            <path d="M18 5h-11h3a4 4 0 0 1 0 8h-3l6 6" />
-                                                            <line x1="7" y1="9" x2="18" y2="9" />
+                                                            <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" />
+                                                            <path d="M12 3v3m0 12v3" />
                                                         </svg>
                                                     </div>
-                                                    <div className='text-4xl font-sans'>47,500</div>
+                                                    <div className='text-4xl font-sans'>1,799</div>
                                                 </div>
                                                 <div className='pb-4 text-sm w-full bg-slate-800'>per month</div>
                                             </div>
                                             <div className='w-full'>
                                                 <div className='bg-slate-200 p-2'>
                                                     <div className='p-2 border-b-2 border-slate-300 text-xl font-semibold'>Package Summary</div>
-                                                    <div className='p-1 border-b-2 border-slate-300'>35 Keywords</div>
-                                                    <div className='p-1 border-b-2 border-slate-300'>35 Citations</div>
+                                                    <div className='p-1 border-b-2 border-slate-300'>120 Keywords</div>
+                                                    <div className='p-1 border-b-2 border-slate-300'>120 Citations</div>
                                                     <div className='p-1 border-b-2 border-slate-300'>375 Backlinks</div>
                                                 </div>
                                                 <div className='bg-slate-100 p-2'>
